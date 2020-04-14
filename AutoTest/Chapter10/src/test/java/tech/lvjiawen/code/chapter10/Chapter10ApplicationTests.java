ac0683453468c87b07f570195d27d261523a7421
daughlas <daughlas@gmail.com>
2020-04-14 23:48:13 +0800

    begin chapter 10 learn spring boot

---
+++ AutoTest/Chapter10/src/test/java/tech/lvjiawen/code/chapter10/Chapter10ApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.lvjiawen.code.chapter10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter10ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
